43b33dd8e2301d78b221e0a9efdfdc25deec111b
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-03-07 12:48:21 +0800

    Add markdown for blog pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,43 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
- // You can delete this file if you're not using it
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+
+  // If you are experiencing issues with the ordering of the posts on the homepage,
+  // replace the `allMarkdownRemark` line below with something like this:
+  // allMarkdownRemark(sort:{fields:[frontmatter___date], order: ASC}) {
+
+  return graphql(`{
+    allMarkdownRemark {
+      edges {
+        node {
+          html
+          id
+          frontmatter {
+            date
+            path
+            title
+            excerpt
+            tags
+          }
+        }
+      }
+    }
+  }`)
+    .then(result => {
+      if (result.errors) {
+        return Promise.reject(result.errors)
+      }
+
+      const posts = result.data.allMarkdownRemark.edges
+
+      posts.forEach(({node}, index) => {
+        createPage({
+          path: node.frontmatter.path,
+          component: blogPostTemplate
+        })
+      })
+    })
+}
+
---
+++ src/pages/2018-03-07-jenkins-pipeline/index.md
@@ -0,0 +1,11 @@
+---
+path: "/jenkins-pipeline"
+date: "2018-03-07"
+title: "Jenkins Pipeline"
+tags: ["jenkins", "pipeline", "CI", "CD"]
+excerpt: "Jenkins Pipeline CI/CD"
+---
+
+## Jenkins Pipelines
+
+Easy CI/CD with Jenkins Pipelines.
