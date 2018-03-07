16bf84b1c6bc91331809526ae74dbf492006bea4
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-03-07 10:48:46 +0800

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Tech Blog</h1>
+    <p>Under construction.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
