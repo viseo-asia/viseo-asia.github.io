9471d7775470d335df00010a7226125bdaa2b6d0
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-03-13 19:47:21 +0800

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
         name: 'src',
         path: `${__dirname}/src`
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-46997328-3'
+      },
+    },
   ]
 };
